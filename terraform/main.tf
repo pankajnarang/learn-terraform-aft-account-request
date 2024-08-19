eb5a9c78c697f2f6553b1d90917f081545620c1b
Pankaj Narang <pnarang@Pankajs-MacBook-Air.local>
2024-08-19 15:41:14 -0400

    adding Production demo01 account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,32 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+
+module "production" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "theopensource007+production@gmail.com"
+    AccountName               = "production-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "theopensource.in+delegatedAdmin@gmail.com"
+    SSOUserFirstName          = "delegated"
+    SSOUserLastName           = "Admin"
+  }
+
+  account_tags = {
+    "CreatedBy " = "PankajNarang"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "SecurityOperations"
+    change_reason       = "AWS Control Tower Account Factory for Terraform demo"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
