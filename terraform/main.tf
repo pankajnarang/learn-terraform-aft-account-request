9be7761e836b7bfe2dfd198cc7da2b0a5ad5fedf
Pankaj Narang <pnarang@Pankajs-MacBook-Air.local>
2024-08-19 16:20:31 -0400

    deleting Sandbox

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,3 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "theopensource007+sandbox@gmail.com"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "theopensource.in+delegatedAdmin@gmail.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "CreatedBy " = "PankajNarang"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "SecurityOperations"
-    change_reason       = "AWS Control Tower Account Factory for Terraform demo"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-
 module "production" {
   source = "./modules/aft-account-request"
 
@@ -47,10 +18,4 @@ module "production" {
     change_requested_by = "SecurityOperations"
     change_reason       = "AWS Control Tower Account Factory for Terraform demo"
   }
-
-  custom_fields = {
-    group = "prod"
-  }
-
-  account_customizations_name = "sandbox"
 }
