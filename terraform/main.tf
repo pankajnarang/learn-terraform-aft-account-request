019b8778cadec83eaddb264475216e6b5782194e
Pankaj Narang <pnarang@Pankajs-MacBook-Air.local>
2024-08-19 17:27:49 -0400

    adding STNO hub account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,21 @@
-# module "sandbox" {
-#   source = "./modules/aft-account-request"
+module "sandbox" {
+  source = "./modules/aft-account-request"
 
-#   control_tower_parameters = {
-#     AccountEmail              = "theopensource007+sandbox@gmail.com"
-#     AccountName               = "sandbox-aft"
-#     ManagedOrganizationalUnit = "Sandbox"
-#     SSOUserEmail              = "theopensource007+sandbox@gmail.com"
-#     SSOUserFirstName          = "Sandbox"
-#     SSOUserLastName           = "AFT"
-#   }
+  control_tower_parameters = {
+    AccountEmail              = "theopensource007+stno@gmail.com"
+    AccountName               = "STNO"
+    ManagedOrganizationalUnit = "Controllers"
+    SSOUserEmail              = "theopensource007+stno@gmail.com"
+    SSOUserFirstName          = "STNO"
+    SSOUserLastName           = "HUB"
+  }
 
-#   account_tags = {
-#     "CreatedBy " = "PankajNarang"
-#   }
+  account_tags = {
+    "CreatedBy " = "PankajNarang"
+  }
 
-#   change_management_parameters = {
-#     change_requested_by = "SecurityOperations"
-#     change_reason       = "AWS Control Tower Account Factory for Terraform demo"
-#   }
-
-#   custom_fields = {
-#     group = "non-prod"
-#   }
-
-#   account_customizations_name = "sandbox"
-# }
+  change_management_parameters = {
+    change_requested_by = "SecurityOperations"
+    change_reason       = "AWSControlTowerAccountFactoryForTerraformDemo"
+  }
+}
