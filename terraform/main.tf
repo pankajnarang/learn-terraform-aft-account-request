e457c3ad6cfe2e4c49f85eb0616800280572a697
Pankaj Narang <pnarang@Pankajs-MacBook-Air.local>
2024-08-20 10:23:16 -0400

    adding demo01

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,32 @@ module "demo" {
 
   account_customizations_name = "sandbox"
 }  
+
+
+module "demo01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "theopensource007+demo01@gmail.com"
+    AccountName               = "demo01"
+    ManagedOrganizationalUnit = "Controllers"
+    SSOUserEmail              = "theopensource007+demo01@gmail.com"
+    SSOUserFirstName          = "theopensource007"
+    SSOUserLastName           = "demo01"
+  }
+
+  account_tags = {
+    "CreatedBy " = "PankajNarang"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "SecurityOperations"
+    change_reason       = "AWSControlTowerAccountFactoryForTerraformDemo"
+  }
+
+   custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}  
