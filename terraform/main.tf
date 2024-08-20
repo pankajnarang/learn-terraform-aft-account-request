95e8ba87aa2080feb68dcf63ee9eb6497b326aee
Pankaj Narang <pnarang@Pankajs-MacBook-Air.local>
2024-08-20 15:03:29 -0400

    removing the demo account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
-module "demo" {
-  source = "./modules/aft-account-request"
+# module "demo" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "theopensource007+demo@gmail.com"
-    AccountName               = "demo"
-    ManagedOrganizationalUnit = "Controllers"
-    SSOUserEmail              = "theopensource007+demo@gmail.com"
-    SSOUserFirstName          = "theopensource007"
-    SSOUserLastName           = "demo"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "theopensource007+demo@gmail.com"
+#     AccountName               = "demo"
+#     ManagedOrganizationalUnit = "Controllers"
+#     SSOUserEmail              = "theopensource007+demo@gmail.com"
+#     SSOUserFirstName          = "theopensource007"
+#     SSOUserLastName           = "demo"
+#   }
 
-  account_tags = {
-    "CreatedBy " = "PankajNarang"
-  }
+#   account_tags = {
+#     "CreatedBy " = "PankajNarang"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "SecurityOperations"
-    change_reason       = "AWSControlTowerAccountFactoryForTerraformDemo"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "SecurityOperations"
+#     change_reason       = "AWSControlTowerAccountFactoryForTerraformDemo"
+#   }
 
-   custom_fields = {
-    group = "non-prod"
-  }
+#    custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "sandbox"
-}  
+#   account_customizations_name = "sandbox"
+# }  
